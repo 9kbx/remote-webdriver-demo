332276ab56e650c2229fb9d8a6e73d7159447e22
9kb <51182838+9kbx@users.noreply.github.com>
2022-04-26 15:24:11 +0800

    12

--- RemoteWebDriverDemo/Program.cs
+++ RemoteWebDriverDemo/Program.cs
@@ -9,6 +9,25 @@ using PuppeteerSharp;
 
 namespace RemoteWebDriverDemo
 {
+
+    // chrome.exe --remote-debugging-port=9222 --user-data-dir="e:\\cache" --no-default-browser-check --proxy-server="ip:port"
+    /*
+     * other args:
+     * --remote-debugging-address="ip"
+     * --no-first-run
+     * --no-sandbox
+     * --no-zygote
+     * --disable-setuid-sandbox
+     * --disable-gpu
+     * --disable-dev-shm-usage
+     * --single-process
+     * --lang="en_US"
+     * --user-agent="Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4892.0 Safari/537.36"
+     * --window-position=x,y
+     * --window-size=w,h
+     * 
+     */
+
     internal class Program
     {
         static void Main(string[] args)
@@ -18,9 +37,9 @@ namespace RemoteWebDriverDemo
             Console.WriteLine("Hello World!");
 
             //BasicUsage();
-            //RemoteUsage();
+            RemoteUsage();
 
-            RecaptchaUsage();
+            //RecaptchaUsage();
 
 
             Console.ReadLine();
@@ -56,39 +75,28 @@ namespace RemoteWebDriverDemo
             // launch chrome on windows via chrome.exe --remote-debugging-port=9222
             // and go to http://localhost:9222/json/version
 
-            // chrome.exe --remote-debugging-port=9222 --user-data-dir="e:\\cache" --no-default-browser-check --proxy-server="ip:port"
-            /*
-             * other args:
-             * --remote-debugging-address="ip"
-             * --no-first-run
-             * --no-sandbox
-             * --no-zygote
-             * --disable-setuid-sandbox
-             * --disable-gpu
-             * --disable-dev-shm-usage
-             * --single-process
-             * --lang="en-GB"
-             */
 
             var options = new ConnectOptions()
             {
                 DefaultViewport = null, // fullscreen
-                
+                IgnoreHTTPSErrors = true,
                 BrowserURL = "http://127.0.0.1:9222"
                 //BrowserURL = "http://192.168.4.180:9222"
             };
 
 
-            var url = "https://www.google.com/";
-            using (var browser = await PuppeteerSharp.Puppeteer.ConnectAsync(options))
+            var url = "https://www.whoer.net/";
+            //var url = "https://www.google.com/";
+            using (var browser = await Puppeteer.ConnectAsync(options))
             {
                 using (var page = await browser.NewPageAsync())
                 {
                     await page.GoToAsync(url);
-                    var searchElm = await page.XPathAsync("//input[@name='q']");
-                    await searchElm[0].TypeAsync("hello world");
+                    //var searchElm = await page.XPathAsync("//input[@name='q']");
+                    //await searchElm[0].TypeAsync("hello world");
                     await page.ScreenshotAsync(@"screenshot-remote.png");
                 }
+                await browser.CloseAsync();
                 browser.Disconnect();
             }
             Console.WriteLine("done!");
